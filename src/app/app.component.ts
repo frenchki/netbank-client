bffccb0f0560cdd14d0d4ca07ad9b481c53b5d06
Kim French <kim.anders.french@gmail.com>
2022-03-10 23:00:59 +0200

    Initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "netbank-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+    title = "netbank-client";
+}
