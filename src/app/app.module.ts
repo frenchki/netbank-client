7eed54d6d8690c0ae24200d2fb172f3ec21b8b78
frenchki <kim.anders.french@gmail.com>
2022-03-27 19:27:31 +0300

    Added ngx-translate library and added basic setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HttpLoaderFactory } from "./shared/utilities/http-loader-factory";
 
 @NgModule({
     declarations: [
@@ -10,6 +13,15 @@ import { AppComponent } from "./app.component";
     ],
     imports: [
         BrowserModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            defaultLanguage: "en",
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        }),
         AppRoutingModule
     ],
     providers: [],
